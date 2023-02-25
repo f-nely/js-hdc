14e7612e8064d358da7dad1a89e9b487701f4ac6
nely de faria <nelly.nf54@gmail.com>
2023-02-25 19:32:16 -0300

    Create string.js

---
+++ js-hdc/string.js
@@ -0,0 +1,10 @@
+let age = 37;
+
+console.log("Hi, my name is Wayne!");
+console.log('I would want to buy a car');
+console.log(`I'm ${age} years old`);
+
+console.log(typeof "Hi, my name is Wayne!");
+console.log(typeof 'I would want to buy a car');
+console.log(typeof `I'm ${age} years old`);
+console.log(typeof Infinity);
