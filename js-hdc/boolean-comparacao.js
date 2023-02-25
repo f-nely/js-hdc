69213164e21bc60865ede28ad07846c61cf9c626
nely de faria <nelly.nf54@gmail.com>
2023-02-25 19:54:13 -0300

    Changed index.html + create boolean-comparacao.js

---
+++ js-hdc/boolean-comparacao.js
@@ -0,0 +1,8 @@
+console.log(1 > 2); // false
+console.log(5 < 10); // true
+console.log(3 >= 3); // true
+console.log(5 <= 4); // false
+console.log(5 == 4); // false 
+console.log('Guardiola' != 'Guardiola'); // false 
+console.log(3 === '3'); // false
+console.log(4 === 4); // true
